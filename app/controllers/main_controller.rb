4130e8cb369742e298d0803d0683f3664c02c237
Boyang niu <boyang.niu@gmail.com>
2014-03-14 22:52:22 -0400

    added jar for live match info

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,5 @@
+require_relative './constants.rb'
+
 class MainController < ApplicationController
 
     def index
@@ -8,5 +10,14 @@ class MainController < ApplicationController
     def riot
     
     end
+
+    def get_live_match
+        summoner = params[:name]
+        returnstring = %x("#{RUNPATH} #{summoner}")
+
+        render json: returnstring
+    end
+    # {"_blueteam":{"Kha\u0027Zix":121,"Rengar":107,"Fiora":114,"Ashe":22,"Zyra":143},"_purpleteam":{"Rengar":107,"Ashe":22,"Lulu":117,"Jarvan IV":59,"Karthus":30}}
+
 
 end
